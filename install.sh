6f428cd9fe9d1bf1c5e6daff48b382af76763001
escaparrac <65442318+escaparrac@users.noreply.github.com>
2023-12-02 20:50:26 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,21 @@
 # This script will install an SRT relay with SRTLA and NOALBS.
 
-#!/usr/bin/env bash
+#!/bin/bash
 
 while true; do
-    read -p "What's your username? " username
+    read -p "What's your username? (ubuntu will match all the tutorial information) " username
     read -p "Is '$username' your correct username? (yes/no) " confirmation
+
     if [ "$confirmation" = "yes" ]; then
         echo "Great! Let's continue with the script."
-        break
-    else
+        break  # Exit the loop since the username is confirmed
+    elif [ "$confirmation" = "no" ]; then
         echo "Oops! Let's try again."
+    else
+        echo "Invalid input. Please enter 'yes' or 'no'. If you want to exit the script, press CTRL + C."
     fi
-
-echo "script continues"
-
 done
+
+# Main script logic comes here
+echo "Executing main script logic..."
+# Add your main script logic below this line
