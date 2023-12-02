c30f3d79b0925df398c90a7b8125e0e7bea3a00f
escaparrac <65442318+escaparrac@users.noreply.github.com>
2023-12-02 20:37:05 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,15 @@
+# This script will install an SRT relay with SRTLA and NOALBS.
+
+#!/usr/bin/env bash
+
+while true; do
+    read -p "What's your username? " username
+    read -p "Is '$username' your correct username? (yes/no) " confirmation
+    if [ "$confirmation" = "yes" ]; then
+        echo "Great! Let's continue with the script."
+        break
+    else
+        echo "Oops! Let's try again."
+    fi
+
+echo "script continues"
