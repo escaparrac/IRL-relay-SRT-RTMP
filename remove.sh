03d6280a596cab37b0914eda8c49e8b78159ef16
escaparrac <juakin.rrpp@gmail.com>
2023-12-12 23:56:44 +0100

    corrected

--- remove.sh
+++ remove.sh
@@ -6,6 +6,16 @@ sudo rm -rf srtla.sh && echo "srtla.sh removed successfully" || echo "srtla.sh n
 sudo rm -rf srt && echo "srt removed successfully" || echo "srt not removed"
 sudo rm -rf srtla && echo "srtla removed successfully" || echo "srtla not removed"
 
+read -p "Do you want to remove the install.sh file? (Type 'yes' and Enter to remove, or press Enter to exit): " answer
+
+if [ "$answer" = "yes" ]; then
+    cd 
+    sudo rm -rf install.sh
+    echo "install.sh file removed."
+else
+    echo "Exiting without removing the install.sh file."
+fi
+
 cd /etc/systemd/system
 sudo systemctl stop sls.service
 sudo systemctl disable sls.service
@@ -17,15 +27,4 @@ sudo systemctl disable srtla.service
 sudo systemctl daemon-reload
 sudo rm -rf srtla.service && echo "srtla.service removed successfully" || echo "srtla.service not removed"
 
-
 echo "SRT + SRTLA Relay Server files removed succesfully"
-
-read -p "Do you want to remove the install.sh file? (Type 'yes' and Enter to remove, or press Enter to exit): " answer
-
-if [ "$answer" = "yes" ]; then
-    cd ~
-    sudo rm -rf install.sh
-    echo "install.sh file removed."
-else
-    echo "Exiting without removing the install.sh file."
-fi
