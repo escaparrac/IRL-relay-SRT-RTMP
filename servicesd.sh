8ec4b10577e5835be686c2d5a43920a47972a51b
escaparrac <juakin.rrpp@gmail.com>
2024-01-26 21:03:36 +0100

    a

--- servicesd.sh
+++ servicesd.sh
@@ -3,6 +3,9 @@
 # Start the first program in the background
 /home/ubuntu/srt-live-server/bin/sls -c /home/ubuntu/srt-live-server/sls.conf &
 
+# Wait for a few seconds to ensure that sls is fully started
+sleep 5
+
 # Start the second program in the background
 /home/ubuntu/srtla/srtla_rec 8383 0.0.0.0 8282 &
 
