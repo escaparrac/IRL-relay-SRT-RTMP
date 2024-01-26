7756a9811699582645877f01eb49ec9581a5b4a3
escaparrac <juakin.rrpp@gmail.com>
2024-01-26 20:44:03 +0100

    a

--- servicesd.sh
+++ servicesd.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-cd /home/ubuntu/srt-live-server/bin/
-./sls -c ../sls.conf &
+cd ./home/ubuntu/srt-live-server/bin/
+./sls -c ../sls.conf
+
 # cd /home/ubuntu/srtla
 # ./srtla_rec 8383 0.0.0.0 8282 &
 wait -n
