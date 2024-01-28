0cdaffa6fd1fdba1dbec78948dcbc77044cb1fbd
escaparrac <juakin.rrpp@gmail.com>
2024-01-28 17:14:14 +0100

    a

--- docker-alpine/entrypoint.sh
+++ docker-alpine/entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Start the first program in the background
-/root/srt-live-server/bin/sls -c /root/srt-live-server/sls.conf &
+sls -c /home/ubuntu/srt-live-server/sls.conf &
 
 # Wait for a few seconds to ensure that sls is fully started
 sleep 5
 
 # Start the second program in the background
-/root/srtla/srtla_rec 8383 0.0.0.0 8282 &
+srtla_rec 8383 0.0.0.0 8282 &
 
 # Keep the script running
 tail -f /dev/null
